ba1516f97c57d65cb33864295e61abb6cb35ae45
nox <noxhsxrk@icloud.com>
2024-11-07 13:13:38 +0700

    feat: update ai_code_reviewer to use relative import path

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
-
 from setuptools import setup, find_packages
 
 setup(
-  name='ai-pre-commit',
-  version='1.0.0',
-  packages=find_packages(),
-  install_requires=[
-      'requests',
-  ],
-  entry_points={
-      'console_scripts': [
-          'ai-pre-commit=ai_code_reviewer:main',
-      ],
-  },
+       name='ai-pre-commit',
+       version='1.0.1',
+       packages=find_packages(),
+       install_requires=[
+           'requests',
+       ],
+       entry_points={
+           'console_scripts': [
+               'ai-pre-commit=ai_pre_commit.ai_code_reviewer:main',
+           ],
+       },
 )
